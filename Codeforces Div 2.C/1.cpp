cc7a8362f38c153ebb36d8de130510cabee0e05d
Shubhanshu <shubhanshu.sv@gmail.com>
2017-06-03 18:57:58 +0530

    Given Length and Sum of Digits...

--- Codeforces Div 2.C/1.cpp
+++ Codeforces Div 2.C/1.cpp
@@ -1,3 +1,5 @@
+// Shubhanshu Verma (SV)
+
 #include <iostream>
 #include <string>
 #include <bits/stdc++.h>
@@ -24,13 +26,29 @@ int main() {
    
    if(s == 0 || s > upper){
 
-      cout << "-1 -1";
+      if(m == 1 && s == 0){
+
+            cout << "0 0" ;
+
+      }else{
+
+            cout << "-1 -1";
+
+      }
+
 
    }else{
 
 
       nines = s/9;
       rem = s - 9*nines;
+
+      if(rem == 0 && nines!= m){
+
+         nines--;
+         rem += 9;
+
+      }
 
       while(nines != count){
 
@@ -60,61 +78,73 @@ int main() {
 
       }else{
 
+         if(m - nines > 1){
 
-         rem--;
-         num[0] = 1;
+               rem--;
+               num[0] = 1;
 
-         num[m-1-count] = rem;
-         count++;
-
-         while(count!=m-1){
-
-               num[m-1-count] = 0;
+               num[m-1-count] = rem;
                count++;
 
+               while(count!=m-1){
+
+                     num[m-1-count] = 0;
+                     count++;
+
+               }
+
+               for(i=0;i<m;i++){
+
+                  cout << num[i];
+
+
+               }
+
+               cout << " ";
+
+               for(i=0;i<nines;i++){
+
+                  cout << "9";
+
+               }
+
+               cout << rem +1;
+               i++;
+
+               for(i = i; i<m;i++){
+
+
+                     cout << '0';
+
+               }
+
+         
+         }else{
+
+
+               num[0] = rem;
+
+               for(i=0;i<m;i++){
+
+                  cout << num[i];
+
+
+               }
+
+               cout << " ";
+
+               for(i=m-1;i>=0;i--){
+
+                  cout << num[i];
+
+
+               }
+
+               cout << " ";
+
          }
-
-         for(i=0;i<m;i++){
-
-            cout << num[i];
-
-
-         }
-
-         cout << " ";
-
-         count = 0;
-
-         for(i=m-1;i>=0;i--){
-
-            if(num[i] != 0){
-
-               cout << num[i];
-
-            }else{
-
-               count++;
-
-            }
-
-         }
-
-         for(i=0;i<count;i++){
-
-            cout << '0';
-
-
-         }
-
-
-
-
 
       }
-
-
-
-
 
    }
 
